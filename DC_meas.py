469a0f12b239e3c8bf47b4a30dde5d43d071b89b
Excalibitar <151876052+Excalibitar@users.noreply.github.com>
2023-12-12 19:23:48 -0600

    Update DC_meas.py

--- DC_meas.py
+++ DC_meas.py
@@ -1,22 +1,15 @@
-from Agilent_DMM_module import Agilent34401A  # Make sure this matches the new name of your module file
+import agilent34401a
 
 def main():
-    # Replace 'GPIB0::25::INSTR' with your DMM's GPIB address
     dmm_address = 'GPIB0::25::INSTR'
-    
-    # Create an instance of the Agilent34401A class
-    dmm = Agilent34401A(dmm_address)
+    dmm = agilent34401a.Agilent34401A(dmm_address)
 
     try:
-        # Take a DC voltage measurement
-        voltage_dc = dmm.measure_voltage_dc()
+        voltage_dc = dmm.measure_voltage_dc(voltage_range='DEF', resolution='MIN')
         print(f"Measured DC Voltage: {voltage_dc} V")
-
     except Exception as e:
         print(f"An error occurred: {e}")
-
     finally:
-        # Always close the connection to the DMM
         dmm.close()
 
 if __name__ == "__main__":
